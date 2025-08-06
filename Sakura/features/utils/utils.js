59e425deeea9b62761f787652529c56200b3e422
Chilee <216861843+wzuz@users.noreply.github.com>
2025-08-06 19:54:42 +0800

    Added getClass and isDpsClass functions

--- Sakura/features/utils/utils.js
+++ Sakura/features/utils/utils.js
@@ -3,3 +3,25 @@ export function isInDungeon() {
         return TabList?.getNames()?.some(a => a.removeFormatting() == 'Dungeon: Catacombs')
     } catch (e) { }
 }
+
+export function getClass() {
+    const scoreboard = Scoreboard.getLines().map(line => line.getName().removeFormatting());
+    const username = Player.getName();
+
+    for (const line of scoreboard) {
+        if (line.includes(username)) {
+            if (line.includes("[A]")) return "archer";
+            if (line.includes("[B]")) return "berserker";
+            if (line.includes("[M]")) return "mage";
+            if (line.includes("[H]")) return "healer";
+            if (line.includes("[T]")) return "tank";
+        }
+    }
+
+    return null;
+}
+
+export function isDpsClass() {
+    const isClass = getClass();
+    return isClass === "archer" || isClass === "berserker" || isClass === "mage";
+}
