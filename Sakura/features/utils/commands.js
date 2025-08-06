1f39c781ffab69dfebaabdb402130421ed685abb
Chilee <216861843+wzuz@users.noreply.github.com>
2025-08-06 20:42:57 +0800

    Added rtca command and requestV2

--- Sakura/features/utils/commands.js
+++ Sakura/features/utils/commands.js
@@ -1,3 +1,5 @@
+const { request } = require("requestV2")
+
 export function handleCommand(args) {
     const command = args[0]?.toLowerCase()
 
@@ -7,14 +9,36 @@ export function handleCommand(args) {
         case "settings":
             return "open_gui"
 
-        case "ping":
-            ChatLib.chat("&5❀ &dSakura &5≫ Pong!")
+         case "ping":
+            ChatLib.chat("&5❀ &dSakura &5≫&f This feature has not been enabled yet.")
+            break
+
+        case "rtca":
+            if (!args[1]) {
+                ChatLib.chat("&cUsage: /sk rtca <username>")
+                break
+            }
+
+            const username = args[1]
+            const url = `https://soopy.dev/api/soopyv2/botcommand?m=rtca&u=${username}`
+
+            request({
+                url: url,
+                method: "GET",
+                text: true
+            }).then(raw => {
+                ChatLib.chat(`&5❀ &dSakura &5≫ &f${raw}.`)
+            }).catch(error => {
+                ChatLib.chat(`&5❀ &dSakura &5≫&c Request error: ${error}`)
+            })
+
             break
 
         case "help":
             ChatLib.chat("&5❀ &dSakura &5≫ &7Available Commands:")
             ChatLib.chat("&b/sk &7- Open the Sakura GUI")
-            ChatLib.chat("&b/sk ping &7- Check if Sakura is active")
+            ChatLib.chat("&b/sk ping &7- Check your current ping")
+            ChatLib.chat("&b/sk rtca <username> &7- Check M7 runs left till Class Average 50")
             ChatLib.chat("&b/sk help &7- Shows this help message")
             break
 
